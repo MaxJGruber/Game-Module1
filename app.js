1fb30a51581e31a404e03f87cd94111d54f9a4b6
MaxJGruber <maxjgruber@gmail.com>
2020-09-07 16:03:36 +0200

    Div elements are now removed once animation ends

--- app.js
+++ app.js
@@ -11,27 +11,31 @@ function randomNote() {
   note.classList.add(
     `${arrayOfCords[Math.floor(Math.random() * arrayOfCords.length)]}`
   );
-  // console.log(note);
-  // console.log(
-  //   Math.floor(
-  //     Math.random() * document.querySelectorAll(".violin-string").length
-  //   )
-  // );
   const chosenParent = document.querySelectorAll(".violin-string")[
     Math.floor(
       Math.random() * document.querySelectorAll(".violin-string").length
     )
   ];
-  console.log(chosenParent);
   chosenParent.appendChild(note);
+  removeElement();
 }
 let intervalId = 0;
 
 function startSong() {
   setInterval(randomNote, 1000);
-  intervalId++;
-  clearInterval(10);
 }
 // setInterval(randomNote, 1000);
-// startSong();
 // randomNote();
+
+// ANIMATION END
+function removeElement() {
+  const animated = document.querySelectorAll(".note");
+  console.log(animated);
+  animated.forEach((element) =>
+    element.addEventListener("animationend", () => {
+      element.parentNode.removeChild(element);
+      console.log("Animation ended");
+    })
+  );
+}
+// startSong();
