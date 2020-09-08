aaf1b58add4bef4bcdadc4ee9fcfeb34c4ef0466
MaxJGruber <maxjgruber@gmail.com>
2020-09-08 16:39:35 +0200

    Interaction zone is now operational

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+// CREATING RANDOM NOTE
+
 const arrayOfNotes = [
   ["first-note", "first-cord", "one"],
   ["second-note", "second-cord", "two"],
   ["third-note", "third-cord", "three"],
   ["fourth-note", "fourth-cord", "four"],
 ];
-// const arrayOfCords = ["first-cord", "second-cord", "third-cord", "fourth-cord"];
-const arrayOfStrings = ["one", "two", "three", "four"];
 
 function randomNote() {
   let note = document.createElement("div");
@@ -23,7 +23,6 @@ function randomNote() {
   chosenParent.appendChild(note);
   removeElement();
 }
-let intervalId = 0;
 
 function startSong() {
   setInterval(randomNote, 1000);
@@ -32,11 +31,9 @@ function startSong() {
 // ANIMATION END
 function removeElement() {
   const animated = document.querySelectorAll(".note");
-  console.log(animated);
   animated.forEach((element) =>
     element.addEventListener("animationend", () => {
-      element.parentElement.removeChild(element);
-      console.log("Animation ended");
+      element.parentNode.removeChild(element);
     })
   );
 }
@@ -45,48 +42,87 @@ function removeElement() {
 
 let elem1 = document.querySelector(".first-btn");
 let rect1 = elem1.getBoundingClientRect();
+console.log(rect1);
 let elem2 = document.querySelector(".second-btn");
 let rect2 = elem2.getBoundingClientRect();
+console.log(rect2);
 let elem3 = document.querySelector(".third-btn");
 let rect3 = elem3.getBoundingClientRect();
+console.log(rect3);
 let elem4 = document.querySelector(".fourth-btn");
 let rect4 = elem4.getBoundingClientRect();
+console.log(rect4);
 
-console.log(typeof rect1, rect2, rect3, rect4);
+// SCORE SET TO ZERO
+
 let score = 0;
-function hitButton() {
-  let firstStringNote = document.querySelector(".first-note");
-  let firstStringRect = firstStringNote.getBoundingClientRect();
-  let secondStringNote = document.querySelector(".second-note");
-  let secondStringRect = secondStringNote.getBoundingClientRect();
-  let thirdStringNote = document.querySelector(".third-note");
-  let thirdStringRect = thirdStringNote.getBoundingClientRect();
-  let fourthStringNote = document.querySelector(".fourth-note");
-  let fourthStringRect = fourthStringNote.getBoundingClientRect();
 
-  if (firstStringRect == rect1) {
-    score += 10;
-    console.log("YOU HIT");
+// CLICKHANDLER
+
+function checkingCoordinates() {
+  let firstStringNote = document.querySelector(".first-note");
+  if (firstStringNote) {
+    let firstStringRect = firstStringNote.getBoundingClientRect();
+    if (firstStringRect.x < rect1.x + 10 && firstStringRect.x > rect1.x - 10) {
+      score += 10;
+    }
   }
-  if (secondStringRect == rect2) {
-    score += 10;
-    console.log("YOU HIT");
+  let secondStringNote = document.querySelector(".second-note");
+  if (secondStringNote) {
+    let secondStringRect = secondStringNote.getBoundingClientRect();
+    if (
+      secondStringRect.x < rect2.x + 10 &&
+      secondStringRect.x > rect2.x - 10
+    ) {
+      score += 10;
+      console.log(secondStringRect);
+    }
   }
-  if (thirdStringRect == rect3) {
-    score += 10;
-    console.log("YOU HIT");
+  let thirdStringNote = document.querySelector(".third-note");
+  if (thirdStringNote) {
+    let thirdStringRect = thirdStringNote.getBoundingClientRect();
+    if (thirdStringRect.x < rect3.x + 10 && thirdStringRect.x > rect3.x - 10) {
+      score += 10;
+      console.log(thirdStringRect);
+    }
   }
-  if (fourthStringRect == rect4) {
-    score += 10;
-    console.log("YOU HIT");
+  let fourthStringNote = document.querySelector(".fourth-note");
+  if (fourthStringNote) {
+    let fourthStringRect = fourthStringNote.getBoundingClientRect();
+    if (
+      fourthStringRect.x < rect4.x + 10 &&
+      fourthStringRect.x > rect4.x - 10
+    ) {
+      score += 10;
+      console.log(fourthStringRect);
+    }
   }
+
+  // SCORE
+  let scoreview = document.getElementById("score-view");
+  scoreview.innerHTML = `${score}`;
 }
 
-rect1.addEventListener("keydown", hitButton);
-rect2.addEventListener("keydown", hitButton);
-rect3.addEventListener("keydown", hitButton);
-rect4.addEventListener("keydown", hitButton);
+// INTERACTIONS / EVENT LISTENERS
 
-// INTERACTION
-
+document
+  .querySelector(".first-btn")
+  .addEventListener("click", checkingCoordinates);
+document
+  .querySelector(".second-btn")
+  .addEventListener("click", checkingCoordinates);
+document
+  .querySelector(".third-btn")
+  .addEventListener("click", checkingCoordinates);
+document
+  .querySelector(".fourth-btn")
+  .addEventListener("click", checkingCoordinates);
 // startSong();
+
+// MUSIC SELECTION
+
+const firstAudioTrack = new Audio("./songs/bensound-goinghigher.mp3");
+const secondAudioTrack = new Audio("./songs/bensound-happyrock.mp3");
+const thirdAudioTrack = new Audio("./songs/bensound-highoctane.mp3");
+const fourthAudioTrack = new Audio("./songs/bensound-punky.mp3");
+const fifthAudioTrack = new Audio("./songs/bensound-rumble.mp3");
