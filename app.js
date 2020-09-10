4f9eb8bfc55f9a220aa4593d1d3a205209a72e43
MaxJGruber <maxjgruber@gmail.com>
2020-09-10 12:56:34 +0200

    start game functional, adding end of game message (still to be implemented), added easter eggs

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 
 let elem1 = document.querySelector(".first-btn");
 let rect1 = elem1.getBoundingClientRect();
+console.log(rect1);
 let elem2 = document.querySelector(".second-btn");
 let rect2 = elem2.getBoundingClientRect();
+console.log(rect2);
 let elem3 = document.querySelector(".third-btn");
 let rect3 = elem3.getBoundingClientRect();
+console.log(rect3);
 let elem4 = document.querySelector(".fourth-btn");
 let rect4 = elem4.getBoundingClientRect();
+console.log(rect4);
 
 // MUSIC SELECTION
 
@@ -27,16 +31,20 @@ const playlist = [
 
 // START BUTTON
 //Main focus of today
-let song = playlist[Math.floor(Math.random() * playlist.length)];
-let applause = new Audio("./songs/applause.mp3");
+// let song = playlist[Math.floor(Math.random() * playlist.length)];
+// let applause = new Audio("./songs/applause.mp3");
 function startSong() {
-  song.play();
-  console.log(song);
-  setInterval(() => {
+  // song.play();
+  // console.log(song);
+  const intervalId = setInterval(() => {
     if (id === 5) {
-      clearInterval(id);
-      song.pause();
-      applause.play();
+      clearInterval(intervalId);
+      id = 0;
+      console.log("BONZOUR");
+      console.log(id);
+      whichWinnerAreYou(score);
+      // song.pause();
+      // applause.play();
     } else {
       randomNote();
     }
@@ -84,14 +92,16 @@ function removeElement() {
   );
 }
 
-// SCORE SET TO ZERO
+// SCORE AND STREAK SET TO ZERO
 
 let score = 0;
+let streak = 0;
 
 // CLICKHANDLER (Interaction of button click; outcome determined by timing (checking coordinates of
 // note element compared to fret-board element coordinates)
 
 function checkingCoordinates(targetClass, targetRect) {
+  let streakview = document.getElementById("streak-view");
   let stringNote = document.querySelector(targetClass);
   if (stringNote) {
     let stringRect = stringNote.getBoundingClientRect();
@@ -102,16 +112,23 @@ function checkingCoordinates(targetClass, targetRect) {
       stringRect.x === targetRect.x
     ) {
       score += 10;
+      streak += 1;
+      streakview.innerHTML = `${streak}`;
+      // whichWinnerAreYou(score);
+      multiplyer(streak);
       console.log("NOTE Y", stringRect.y);
       console.log("BUTTON Y", targetRect.y);
       console.log("BUTTON Y+20", targetRect.y + 20);
       console.log("RESULT", stringRect.y, targetRect.y);
       console.log("RESULT", stringRect.x, targetRect.x);
+    } else {
+      streak = 0;
     }
   }
 }
 
 function hitButton(evtClass) {
+  // const currentCssClass = evt.target.className.replace("tap-button ", "");
   const currentCssClass = evtClass;
   const possibleActions = {
     "first-btn": [rect1, ".first-note"],
@@ -129,30 +146,76 @@ function hitButton(evtClass) {
   scoreview.innerHTML = `${score}`;
 }
 
+function multiplyer(winStreak) {
+  let multiplyerview = document.getElementById("multiplyer-view");
+
+  switch (winStreak) {
+    case 4:
+      score += 20;
+      multiplyerview.innerHTML = `X2`;
+      break;
+    case 6:
+      score += 40;
+      multiplyerview.innerHTML = `X4`;
+      break;
+    case 8:
+      score += 80;
+      multiplyerview.innerHTML = `X8`;
+      break;
+    case 0:
+      score += 10;
+      multiplyerview.innerHTML = `0`;
+  }
+}
+
+// function whichWinnerAreYou(actualScore) {
+//   let rank = document.getElementById("rank");
+//   switch (actualScore) {
+//     case actualScore > 50:
+//       rank.innerHTML = `ROADIE!`;
+//       console.log(`ROADIE!`);
+//       break;
+//     case actualScore > 1000:
+//       rank.innerHTML = `ZAYN MALIK?`;
+//       console.log(`ZAYN MALIK?`);
+//       break;
+//     case actualScore > 2000:
+//       rank.innerHTML = `SUPERSTAR!`;
+//       console.log(`SUPERSTAR!`);
+//       break;
+//     case actualScore > 3000:
+//       rank.innerHTML = `LEGEND!`;
+//       console.log(`LEGEND!`);
+//       break;
+//     case actualScore > 4000:
+//       rank.innerHTML = `ROCK GOD!`;
+//       console.log(`ROCK GOD!`);
+//       break;
+//   }
+// }
 // INTERACTIONS / EVENT LISTENERS
 
 document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "a":
-      console.log("hello");
       hitButton("first-btn");
       break;
     case "s":
-      console.log("hello");
       hitButton("second-btn");
       break;
     case "d":
-      console.log("hello");
       hitButton("third-btn");
       break;
     case "f":
-      console.log("hello");
       hitButton("fourth-btn");
       break;
   }
 });
 
 document.getElementById("play-btn").addEventListener("click", startSong);
+document.getElementById("play-again-btn").addEventListener("click", startSong);
+
+// CLICKING VERSION
 
 // document.querySelector(".first-btn").addEventListener("click", hitButton);
 // document.querySelector(".second-btn").addEventListener("click", hitButton);
